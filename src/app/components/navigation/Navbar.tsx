9fa10ce108609f282cd864dbe4e99ed8d249ed9a
N-Crossman <noahc1@hotmail.com>
2025-12-09 14:44:28 -0700

    File and structure setup

---
+++ src/app/components/navigation/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/players">Players</a></li>
+        <li><a href="/teams">Teams</a></li>
+        <li><a href="/stats">Stats</a></li>
+      </ul>
+    </nav>
+  );
+}
