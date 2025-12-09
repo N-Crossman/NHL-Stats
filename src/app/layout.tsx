5e532aa0f2c100f363aa8899d0029ea18a8bbabd
N-Crossman <noahc1@hotmail.com>
2025-12-09 15:13:43 -0700

    Import Footer.tsx into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navigation/Navbar";
+import Footer from "./components/navigation/Footer";
 
 export const metadata: Metadata = {
   title: "NHL Stats App",
@@ -19,6 +20,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
